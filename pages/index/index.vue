5f4276568c07f9900cf8adbb00d66789c7b61821
xiewu <xiewu@cmcm.com>
2019-05-16 15:29:43 +0800

    首页超英预告请求完成提交

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,7 @@
 <template>
 	<view class="page">
+		
+		<!-- 轮播图 S -->
 		<swiper class="swiper_con" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
 			<block :key="item.id" v-for="item in carouseList">
 				<swiper-item class="swiper_item" v-if="item.isShow">
@@ -7,6 +9,34 @@
 				</swiper-item>
 			</block>
 		</swiper>
+		<!-- 轮播图 E -->
+		
+		<!-- 热门超英 S -->
+		<view class="page-block super-hot">
+			<view class="hot-title-wapper">
+				<image src="../../static/icos/hot.png" class="hot-ico" />
+				<view class="hot-title">热门超英</view>
+			</view>
+		</view>
+		<scroll-view scroll-x="true" class="page-block hot">
+			<view class="single-poster">
+				<view class="poster-wapper">
+					<image src="../../static/poster/civilwar.jpg" class="poster"/>
+					<view class="movie-name">蝙蝠侠蝙蝠侠蝙蝠侠蝙蝠侠</view>
+					<view class="movie-score-wapper">
+						<image src="../../static/icos/star-yellow.png" class="star-ico" />
+						<image src="../../static/icos/star-yellow.png" class="star-ico" />
+						<image src="../../static/icos/star-yellow.png" class="star-ico" />
+						<image src="../../static/icos/star-yellow.png" class="star-ico" />
+						<image src="../../static/icos/star-yellow.png" class="star-ico" />
+						<view class="movie-score">9.1</view>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
+		<!-- 热门超英 E -->
+		
+		
 	</view>
 </template>
 
@@ -16,12 +46,16 @@
 	export default {
 		data() {
 			return {
-				carouseList:[]
+				carouseList:[],
+				hotSuperList:[]
 			}
 		},
 		async onLoad() {
 			let carouseList = await superRequest.post("/index/carousel/list");
 			this.carouseList = carouseList;
+			let hotSuperList = await superRequest.post("/index/movie/hot",{type:"superhero"});
+			this.hotSuperList = hotSuperList; 
+			console.log(hotSuperList);
 		},
 		methods: {
 
