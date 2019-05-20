aa98bb9a88a10ae8fb7a8a8abfe841c8a68d2fda
xw5 <412832768@qq.com>
2019-05-21 00:37:57 +0800

    电影详情页完成提交

--- pages/movie/movie.vue
+++ pages/movie/movie.vue
@@ -51,6 +51,71 @@
 			<view class="plots-desc">{{trailerInfo.plotDesc}}</view>
 		</view>
 		<!-- 剧情介绍 E-->
+		
+		<!-- 演职人员 S-->
+		<view class="plots-block">
+			<view class="plots-title">演职人员</view>
+			<scroll-view scroll-x class="scroll-list">
+				<view
+				class="actor-wapper"
+				v-for="(staff, index) in staffPhoto"
+				:key="staff.staffId"
+				@click="lookStaff"
+				:data-index="index"
+				>
+					<image
+					class="single-actor"
+					mode="aspectFill"
+					:src="staff.photo"></image>
+					<view class="actor-name">{{staff.name}}</view>
+					<view class="actor-role">{{staff.actName}}</view>
+				</view>
+				
+				<!-- <view
+				class="actor-wapper"
+				v-for="director in directorArray"
+				:key="director.staffId"
+				>
+					<image
+					class="single-actor"
+					mode="aspectFill"
+					:src="director.photo"></image>
+					<view class="actor-name">{{director.name}}</view>
+					<view class="actor-role">{{director.actName}}</view>
+				</view>
+				
+				<view
+				class="actor-wapper"
+				v-for="actor in actorArray"
+				:key="actor.staffId"
+				>
+					<image
+					class="single-actor"
+					mode="aspectFill"
+					:src="actor.photo"></image>
+					<view class="actor-name">{{actor.name}}</view>
+					<view class="actor-role">{{actor.actName}}</view>
+				</view> -->
+				
+			</scroll-view>
+		</view>
+		<!-- 演职人员 E-->
+		
+		<!-- 剧照 S-->
+		<view class="plots-block">
+			<view class="plots-title">剧照</view>
+			<scroll-view scroll-x class="scroll-list">
+				<image
+				class="plot-image"
+				v-for="(img, index) in plotPicsArray"
+				:key="index"
+				mode="aspectFill"
+				@click="lookMe"
+				:data-index="index"
+				:src="img"></image>
+			</scroll-view>
+		</view>
+		<!-- 剧照 E-->
 	</view>
 </template>
 
@@ -60,16 +125,43 @@
 	export default {
 		data() {
 			return {
-				trailerInfo:{}
+				trailerInfo:{},
+				plotPicsArray:[],
+				directorArray:[],
+				actorArray:[]
 			}
 		},
 		async onLoad(params) {
-			console.log(params);
+			console.log("传入的参数：",params);
 			let trailerInfo = await superRequest.post("/search/trailer/"+params.trailerId);
 			this.trailerInfo = trailerInfo;
+			this.plotPicsArray = JSON.parse(trailerInfo.plotPics);
+			let directorsArray = await superRequest.post(`/search/staff/${params.trailerId}/1`);
+			this.directorArray=directorsArray;
+			let actorArray = await superRequest.post(`/search/staff/${params.trailerId}/2`);
+			this.actorArray=actorArray;
 		},
 		methods: {
-			
+			// 查看剧照
+			lookMe(e) {
+				uni.previewImage({
+					urls:this.plotPicsArray,
+					current:e.currentTarget.dataset.index
+				})
+			},
+			// 查看演职列表
+			lookStaff(e) {
+				let staffPhoto = this.staffPhoto.map(item => item.photo);
+				uni.previewImage({
+					urls:staffPhoto,
+					current:e.currentTarget.dataset.index
+				})
+			}
+		},
+		computed:{
+			staffPhoto() {
+				return this.directorArray.concat(this.actorArray);
+			}
 		},
 		components:{
 			TrailerStars
